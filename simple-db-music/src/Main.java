e5a8fb39e22ea2ebc587e343bcca3348714fa2cf
Bryan Williams <2016bmw@csail.mit.edu>
2015-11-21 17:44:54 -0500

    Switched over to using new musicg library to extract spectrogram data and perform optional visualization. Code now a lot cleaner

--- simple-db-music/src/Main.java
+++ simple-db-music/src/Main.java
@@ -1,72 +1,14 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-import visual.Spectrogram;
-
 public class Main {
+    
+    private static final String SONG_NAME = "hi_bryan.wav";
 	
 	public static void main(String[] args) throws UnsupportedAudioFileException {
-		// File f = new File("sample_music/06 Melt My Heart To Stone.mp3");
-	    // File f = new File("sample_music/00 clique_mp3.mp3");
-		// File f = new File("sample_music/Drake_-_Hotline_Bling.mp3");
-	    // File f = new File("sample_music/00 Requiem for a dream Instrumental_mp3.mp3");
-		// ReadAudio.checkFFT();
-	    
 		try {
-		    //ChunkProcessor c = new ChunkProcessor(f, 4096);
-		    //File createdFile = ReadAudio.fft(f);
-		    File createdFile = new File("00 Requiem for a dream Instrumental_mp3.fft");
-		    Spectrogram s = new Spectrogram(createdFile, 4096);
-		    s.show();
-		    
-			//double[][] results = ReadAudio.fft(f);
-		    /*
-            double[][] magnitudes = new double[results.length][ReadAudio.CHUNK_SIZE];
-			
-			double real, complex;
-			for (int i = 0; i < results.length; i++) {
-			    for (int j = 0; j < ReadAudio.CHUNK_SIZE; j++) {
-			        real = results[i][2*j];
-			        complex = results[i][2*j+1];
-			        magnitudes[i][j] = Math.sqrt(real*real+complex*complex);
-			    }
-			}
-			
-			System.out.println("detecting duplicates...");
-			boolean same;
-			boolean everSame = false;
-			for (int i = 0; i < magnitudes.length; i++) {
-			    for (int j = i+1; j < magnitudes.length; j++) {
-			        same = true;
-			        for (int k = 0; k < magnitudes[i].length; k++) {
-			            if (magnitudes[i][k] != magnitudes[j][k]) {
-			                same = false;
-			                break;
-			            }
-			        }
-			        if (same) {
-			            everSame = true;
-			            System.out.println(i+" and "+j+" are duplicates!!");
-			        }
-			    }
-			}
-			
-			System.out.println("everSame? "+everSame);
-			
-			
-			System.out.println(magnitudes[0][372]);
-			
-	        System.out.println(magnitudes[1838][349]);
-	        */
-
-			/*
-			for (int i = 0; i < 10; i++) {
-			    System.out.println(Arrays.toString(magnitudes[i]));
-			}
-			*/			
+		    double[][] spectrogram = ReadAudio.extractSpectogram("sample_music/"+SONG_NAME, true);
+		    System.out.println("num lines: "+spectrogram.length);
+		    System.out.println("num freqs in line "+spectrogram[0].length);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- simple-db-music/src/ReadAudio.java
+++ simple-db-music/src/ReadAudio.java
@@ -1,179 +1,31 @@
-import java.io.BufferedWriter;
-import java.io.DataOutputStream;
-import java.io.*;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.Writer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
-import edu.emory.mathcs.jtransforms.fft.DoubleFFT_1D;
+import com.musicg.dsp.Resampler;
+import com.musicg.graphic.GraphicRender;
+import com.musicg.wave.Wave;
+import com.musicg.wave.WaveHeader;
+import com.musicg.wave.extension.Spectrogram;
 
 public class ReadAudio {
+	private static final int SAMPLE_RATE = 44100;
 	
-	public static final int CHUNK_BYTE_SIZE = 4096;
-	private static final int CHUNK_BATCH_SIZE = 1000;
-//	
-//	private static byte[] toByteArray(File audioFile) throws IOException, UnsupportedAudioFileException {		
-//
-//		int curByte = din.read();
-//		List<Integer> byteList = new ArrayList<>();
-//		while (curByte!= -1) {
-//		    byteList.add(curByte);
-//		    curByte = din.read();
-//		}
-//		in.close();
-//		
-//		byte[] bytes = new byte[byteList.size()];
-//		for (int i = 0; i < bytes.length; i++) {
-//		    bytes[i] = byteList.get(i).byteValue();
-//		}
-//		
-//		return bytes;
-//	}
-	
-
-	public static File fft(File audioFile) throws IOException, UnsupportedAudioFileException {
-//		byte[] bytes = ReadAudio.toByteArray(audioFile);
-//		System.out.println("Successfully read file");;
-
-	    String fullName = audioFile.getName();
-	    File f = new File(fullName.substring(0, fullName.indexOf('.'))+".fft");
-	    if (f.exists()) {
-	        f.delete();
+	public static double[][] extractSpectogram(String filename, boolean renderSpectrogram) {
+	    Wave wave = new Wave(filename);
+	    
+	    Resampler resampler = new Resampler();
+	    int sourceRate = wave.getWaveHeader().getSampleRate();
+	    byte[] resampledBytes = resampler.reSample(wave.getBytes(), wave.getWaveHeader().getBitsPerSample(), 
+	            sourceRate, SAMPLE_RATE);
+	    
+	    WaveHeader resampledHeader = wave.getWaveHeader();
+	    resampledHeader.setSampleRate(SAMPLE_RATE);
+	    Wave resampledWave = new Wave(resampledHeader, resampledBytes);
+	    Spectrogram spectrogram = resampledWave.getSpectrogram();
+	    
+	    if (renderSpectrogram) {
+	        GraphicRender render = new GraphicRender();
+	        filename = filename.substring(filename.indexOf('/')+1);
+	        render.renderSpectrogramData(spectrogram.getNormalizedSpectrogramData(), "out/"+filename+"_norm.jpg");
 	    }
-	    f.createNewFile();
-	    DataOutputStream outputStream = new DataOutputStream(new FileOutputStream(f, true));
-	            /*
-	    try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-	              new FileOutputStream("filename.txt"), "utf-8"))) {
-	   writer.write("something");
+	    	    
+	    return spectrogram.getAbsoluteSpectrogramData();
 	}
-	*/
-	    
-	    
-	    ChunkProcessor cp = new ChunkProcessor(audioFile, CHUNK_BYTE_SIZE);
-	    
-        DoubleFFT_1D fftDo = new DoubleFFT_1D(CHUNK_BYTE_SIZE);
-	    List<double[]> curBatch = new ArrayList<double[]>();
-	    double[] curLine = new double[CHUNK_BYTE_SIZE*2];
-	    List<Byte> chunkBytes = new ArrayList<Byte>();
-	    int count = 0;
-	    while (cp.hasChunksLeft()) {
-	        curBatch.clear();
-	        for(int i = 0; i < CHUNK_BATCH_SIZE; i++) {
-	            count++;
-	            if (count % 100 == 0) {
-	                System.out.println("cur chunk: "+count);
-	            }
-	            if (!cp.hasChunksLeft()) {
-	                break;
-	            }
-	            chunkBytes = cp.getNextChunk();
-	            // Filter out incomplete chunks for now
-	            if (chunkBytes.size() < CHUNK_BYTE_SIZE) {
-	                break;
-	            }
-	            for (int j = 0; j < CHUNK_BYTE_SIZE; j++) {
-	                curLine[j] = chunkBytes.get(j);
-	            }
-	            // Fill the second half of curLine with 0s - these represent complex numbers
-	            for (int j = CHUNK_BYTE_SIZE; j < CHUNK_BYTE_SIZE*2; j++) {
-	                curLine[i] = 0;
-	            }
-	            fftDo.realForwardFull(curLine);
-	            curBatch.add(Arrays.copyOf(curLine, curLine.length));
-	        }
-	        System.out.println("writing to file...");
-	        writeOutBatch(curBatch, outputStream);
-	    }
-	    outputStream.flush();
-	    outputStream.close();
-	    return f;
-	    
-	    /*
-		double[][] results = new double[numChunks][CHUNK_SIZE*2];
-        double[] dub = new double[CHUNK_SIZE*2];
-
-        //double[] input = new double[CHUNK_SIZE*2];
-        
-		for(int times = 0; times < numChunks; times++) {
-			for(int i = 0; i < CHUNK_SIZE; i++) {
-				//Put the time domain data into a complex number with imaginary part as 0
-			    
-				//complex[i] = new Complex(bytes[(times*CHUNK_SIZE)+i], 0);
-			    dub[i] = bytes[(times*CHUNK_SIZE)+i];
-			    //dub[i+CHUNK_SIZE] = dub[i];
-			}
-	        for (int i = CHUNK_SIZE; i < CHUNK_SIZE*2; i++) {
-	            dub[i] = 0;
-	        }
-	        
-			//Perform FFT analysis on the chunk:
-			//results[times] = FFT.fft(complex);
-			fftDo.realForwardFull(dub);
-			//results[times] = Arrays.copyOf(dub, CHUNK_SIZE*2);
-			for (int i = 0; i < CHUNK_SIZE*2; i++) {
-			    results[times][i] = dub[i];
-			}
-		}
-		
-		return results;
-		*/
-	}
-	
-	private static void writeOutBatch(List<double[]> batch, DataOutputStream outputStream) throws IOException {
-	    for (double[] line : batch) {
-	        for (int i = 0; i < CHUNK_BYTE_SIZE*2; i++) {
-	            outputStream.writeDouble(line[i]);
-	        }
-	    }
-	    //outputStream.flush();
-	}
-	
-	/*
-	public static void fft2(File audioFile) throws IOException, UnsupportedAudioFileException {
-		byte[] bytes = ReadAudio.toByteArray(audioFile);
-		System.out.println("Successfully read file");
-		int totalSize = bytes.length;
-		int numChunks = totalSize / CHUNK_SIZE;
-		double[][] audioRe = new double[numChunks][];
-		double[][] audioIm = new double[numChunks][];
-		double[][] audioMag = new double[numChunks][];
-		FFT2 fft = new FFT2(CHUNK_SIZE);
-		double[] real = new double[CHUNK_SIZE];
-		double[] imaginary =  new double[CHUNK_SIZE];
-		for(int times = 0; times < numChunks; times++) {
-			for(int i = 0; i < CHUNK_SIZE; i++) {
-				//Put the time domain data into a complex number with imaginary part as 0
-				real[i] = bytes[(times*CHUNK_SIZE)+i];
-				imaginary[i] = 0;
-			}
-			fft.fft(real, imaginary);
-			audioRe[times] =  real;
-			audioIm[times] = imaginary;
-			double[] magnitudes = new double[CHUNK_SIZE];
-			for (int i = 0; i < CHUNK_SIZE; i++) {
-				magnitudes[i] = Math.sqrt(real[i] * real[i] + imaginary[i] * imaginary[i]);
-			}
-			audioMag[times] = magnitudes;
-		}
-		System.out.println("Finished FFT");
-	}
-	*/
-
-	public static void checkFFT() {
-	    double[] input = {0, 1, 2, 3, 4, 5, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0};
-        DoubleFFT_1D fftDo = new DoubleFFT_1D(8);
-	    fftDo.realForwardFull(input);
-	    System.out.println(Arrays.toString(input));
-	}
-	
 }
