7356b525c7d61b3dc975e959a090028a425eb3c7
Bryan Williams <2016bmw@csail.mit.edu>
2015-11-23 22:52:06 -0500

    Removed stale code

--- simple-db-music/src/fingerprint/DataPoint.java
+++ simple-db-music/src/fingerprint/DataPoint.java
@@ -60,31 +60,4 @@ public class DataPoint {
             return false;
         return true;
     }
-    
-    
-
-    /*
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + hash;
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        DataPoint other = (DataPoint) obj;
-        if (hash != other.hash)
-            return false;
-        return true;
-    }
-    */
-    
 }
